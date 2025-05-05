cd853bc15bce5f2635397c1e728cd727b649625e
Graziela00 <grazy3634@gmail.com>
2025-05-04 23:48:53 -0300

    Implementaçao dos metodos da aula 4

--- src/main/java/exercicios/Aula04.java
+++ src/main/java/exercicios/Aula04.java
@@ -2,75 +2,74 @@ package exercicios;
 
 import exercicios.base.Aula;
 import lombok.NonNull;
-
+import java.util.OptionalDouble;
 import java.util.stream.Stream;
 
 /**
- * Esta é uma classe para você poder implementar as atividades propostas no README.
- * Você <b>NÃO</b> deve alterar:
+ * EstaéumaclassaparavocêpoderimplementarasatividadespropostasnoREADME.
+ * Você<b>NÃO</b>devealterar:
  * <ul>
- *     <li>a estrutura deste arquivo;</li>
- *     <li>o nome da classe, dos métodos ou dos atributos;</li>
- *     <li>parâmetros e tipo de retorno dos métodos.</li>
+ * <li>aestruturadestearquivo;</li>
+ * <li>onomedaclasse,dosmétodosoudosatributos;</li>
+ * <li>parâmetrosetipoderetornodosmétodos.</li>
  * </ul>
  *
- * <p>Você pode alterar o código interno dos métodos, criar métodos auxiliares que podem ser chamados
- * pelos existentes, mas não deve alterar a estrutura dos métodos disponíveis.</p>
+ * <p>Vocêpodealterarocódigointernodosmétodos,criarmétodosauxiliaresquepodemserchamados
+ * pelosexistentes,masnãodevealteraraestruturadosmétodosdisponíveis.</p>
  *
- * @author Manoel Campos da Silva Filho
+ * @authorManoelCamposdaSilvaFilho
  */
 public class Aula04 extends Aula {
 
     /**
-     * Você pode chamar os métodos existentes e outros que você criar aqui,
-     * incluir prints e fazer o que desejar neste método para conferir os valores retornados pelo seu método.
-     * Para verificar se sua implementação está correta, clique com o botão direito no nome do projeto na aba esquerda
-     * do IntelliJ e selecione a opção "Run All Tests".
+     * Vocêpodechamarosmétodosexistenteseoutrosquevocêcriaraqui,
+     * incluirprintsefazeroquedesejarnestemétodoparaconferirosvaloresretornadospeloseumétodo.
+     * Paraverificarsesuaimplementaçãoestácorreta,cliquecomobotãodireitononomedoprojetonaabaesquerda
+     * doIntelliJeselecioneaopção"RunAllTests".
      */
     public Aula04() {
         final var curso = generator.CURSOS[3];
         final char homem = 'M';
         final char mulher = 'F';
 
-        System.out.printf("Maior nota de todos os Estudantes: %.2f%n", maiorNotaTodosEstudantes(estudantes.stream()));
-        System.out.printf("Maior nota dos Estudantes homens: %.2f%n", maiorNotaHomens(estudantes.stream()));
-        System.out.printf("Maior nota das mulheres do curso de %s: %.2f%n", curso.getNome(), maiorNotaCursoAndSexo(estudantes.stream(), curso, mulher));
-        System.out.printf("Média de notas dos Estudantes do curso de %s: %.2f%n", curso.getNome(), mediaNotaTodosEstudantesCurso(estudantes.stream(), curso));
-        System.out.printf("Total dos homens do curso de %s: %d%n", curso.getNome(), totalEstudantesCursoAndSexo(estudantes.stream(), curso, homem));
-        System.out.printf("Total das mulheres do curso de %s: %d%n", curso.getNome(), totalEstudantesCursoAndSexo(estudantes.stream(), curso, mulher));
+        System.out.printf("MaiornotadetodososEstudantes:%.2f%n", maiorNotaTodosEstudantes(estudantes.stream()));
+        System.out.printf("MaiornotadosEstudanteshomens:%.2f%n", maiorNotaHomens(estudantes.stream()));
+        System.out.printf("Maiornotadasmulheresdocursode%s:%.2f%n", curso.getNome(), maiorNotaCursoAndSexo(estudantes.stream(), curso, mulher));
+        System.out.printf("MédiadenotasdosEstudantesdocursode%s:%.2f%n", curso.getNome(), mediaNotaTodosEstudantesCurso(estudantes.stream(), curso));
+        System.out.printf("Totaldoshomensdocursode%s:%d%n", curso.getNome(), totalEstudantesCursoAndSexo(estudantes.stream(), curso, homem));
+        System.out.printf("Totaldasmulheresdocursode%s:%d%n", curso.getNome(), totalEstudantesCursoAndSexo(estudantes.stream(), curso, mulher));
     }
 
     /**
-     * Veja o método construtor {@link #Aula04()}.
+     * Vejaométodoconstrutor{@link#Aula04()}.
      */
     public static void main(String[] args) {
         new Aula04();
     }
-
     protected double maiorNotaCursoAndSexo(@NonNull final Stream<Estudante> stream, @NonNull final Curso curso, final char sexo) {
-        // TODO: Você precisa implementar este método. Apague estas linhas e escreva o código correto.
-        return -1;
+        return stream.filter(e -> e.getCurso() != null && e.getCurso().equals(curso) && e.getSexo() == sexo)
+                .mapToDouble(Estudante::getNota)
+                .max().orElse(-1);
     }
 
     protected long totalEstudantesCursoAndSexo(@NonNull final Stream<Estudante> stream, @NonNull final Curso curso, final char sexo) {
-        // TODO: Você precisa implementar este método. Apague estas linhas e escreva o código correto.
-        return -1;
+        return stream.filter(e -> e.getCurso() != null && e.getCurso().equals(curso) && e.getSexo() == sexo).count();
     }
 
     protected double mediaNotaTodosEstudantesCurso(@NonNull final Stream<Estudante> stream, @NonNull final Curso curso){
-        // TODO: Você precisa implementar este método. Apague estas linhas e escreva o código correto.
-        return -1;
+        OptionalDouble average = stream.filter(e -> e.getCurso() != null && e.getCurso().equals(curso))
+                .mapToDouble(Estudante::getNota)
+                .average();
+        return average.isPresent() ? average.getAsDouble() : -1;
     }
 
     protected double maiorNotaTodosEstudantes(@NonNull final Stream<Estudante> stream){
-        // TODO: Você precisa implementar este método. Apague estas linhas e escreva o código correto.
-        return -1;
+        return stream.mapToDouble(Estudante::getNota).max().orElse(-1);
     }
-
 
     protected double maiorNotaHomens(@NonNull final Stream<Estudante> stream){
-        // TODO: Você precisa implementar este método. Apague estas linhas e escreva o código correto.
-        return -1;
+        return stream.filter(e -> e.getSexo() == 'M')
+                .mapToDouble(Estudante::getNota)
+                .max().orElse(-1);
     }
 }
-
